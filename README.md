0e5e966e103ea6e8a8e6381160f4dd65e2144390
Arthur Fiorese <arthur.herrada@gmail.com>
2026-09-17 09:57:13 -0300

    feat: Codificando as funcionalidades do pedido

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 ## FILTER
 ## REDUCE
 
-> MAP - Faz uma cópia e mexe nessa cópia, e deixa o original intacto. Cria um novo array sem modificar o original.
+> MAP: Percorre a lista para criar um novo array sem modificar o original.
 
 > FILTER - Cria um novo array e deixa somente o que será alterado.
 
-> REDUCE - Realiza soma
+> REDUCE - Calcula a soma
--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -6,7 +6,7 @@ const cardapio = [
     {id: 2, nome: "Combo-02", preco: 30.00, disponivel: true, quantidade:0},
     {id: 3, nome: "Combo-03", preco: 35.00, disponivel: false, quantidade:0},
     {id: 4, nome: "Combo-04", preco: 40.00, disponivel: true, quantidade:0},
-]
+];
 
 const Pedido = () => {
 
@@ -31,22 +31,77 @@ const Pedido = () => {
                item.id===id ? {...item, quantidade:Math.max(0,item.quantidade + valor)}:item 
             )
         )
-    }
+    };
 
     // FILTER: Seleciona apenas os produtos disponíveis e do carrinho
     const produtosDisponiveis = items.filter(item => item.disponivel);
-    const carrinho = items.filter(item.quantidade > 0);
+    const carrinho = items.filter(item => item.quantidade > 0);
 
     // REDUCE: Calcula a soma dos itens (preço * quantidade) e adiciona a taxa de entrega
-    const subTotal = carrinho.reduce((ac,item) => ac.item.preco * item.quantidade,0);
+    const subTotal = carrinho.reduce((ac,item) => ac + item.preco * item.quantidade,0);
     const total = subTotal > 0 ? subTotal + taxaEntrega : 0;
 
 
+    // SIMULAÇÃO DO CICLO DE VIDA DA ENTREGA USANDO TEMPORIZADORES ASSÍNCRONOS
+    const confirmarPedido = () => {
+        setEnviar(true);
+        setStatus("Restaurante preparando seu pedido!");
+
+        setTimeout(() => {
+            setStatus("Seu pedido saiu para entrega!");
+            setEnviar(false);
+        }, 5000) // 5 Segundos
+        setTimeout(() =>{
+            setStatus("Seu pedido foi entregue com sucesso!");
+            setEnviar(false);
+        }, 10000) // 10 Segundos
+    };
+
 
   return (
-    <>
-      
-    </>
+    <div>
+        <h1>Cardápio do Restaurante</h1>
+        {produtosDisponiveis.map(produto=>(
+            <div key={produto.id}>
+                <span>{produto.nome}(R${produto.preco.toFixed(2)})</span>
+                <div>
+                    <button onClick={() => AlterarQuantidade(produto.id, -1)}>-</button>
+                    <span>{produto.quantidade}</span>
+                    <button onClick={() => AlterarQuantidade(produto.id, +1)}>+</button>
+                </div>
+            </div>
+        ))}
+
+        <hr></hr>
+        <h3>Resumo da entrega</h3>
+        {carrinho.length === 0 ?(
+            <p>Seu carrinho está vazio!</p>
+        ):(
+            <>
+                <ul>
+                    {carrinho.map(item =>(
+                        <li key={item.id}>
+                            {item.quantidade} X {item.nome} - R$ {(item.preco * item.quantidade).toFixed(2)}
+                        
+                        </li>
+                    ))}
+                </ul>
+                <p>SubTotal: R${subTotal.toFixed(2)}</p>
+                      <p>Taxa de Entrega: R$ {taxaEntrega.toFixed(2)}</p>
+                      <p>Total: R${subTotal + taxaEntrega}</p>
+                <button onClick={confirmarPedido} disabled={enviar}>
+                    {enviar ? "Enviando..." : "Confirmar Pedido"}
+                </button>
+            </>
+        )}
+        {status && (
+            <div>
+                <strong>Alerta: </strong>{status}
+            </div>
+        )}
+
+
+    </div>
   )
 }
 
